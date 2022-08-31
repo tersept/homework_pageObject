9d6ae102e1de83b033e912de04515fd2c19e5cb9
Irina <pashaginy@rambler.ru>
2022-08-31 21:28:34 +0300

    logger add

--- src/test/java/com/demoqa/pages/TestBase.java
+++ src/test/java/com/demoqa/pages/TestBase.java
@@ -2,9 +2,11 @@ package com.demoqa.pages;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.components.CalendarComponent;
 import com.demoqa.pages.components.FileUploadComponent;
 import com.demoqa.pages.components.ResultsModalComponent;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Condition.text;
@@ -14,7 +16,10 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestBase {
     @BeforeAll
     static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
