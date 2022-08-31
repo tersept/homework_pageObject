bb7413c9ac4b0c557d6815f52659b8d09f0920bf
Irina <pashaginy@rambler.ru>
2022-08-31 22:01:00 +0300

    add screnshott

--- src/test/java/com/demoqa/pages/TestBase.java
+++ src/test/java/com/demoqa/pages/TestBase.java
@@ -8,6 +8,7 @@ import com.demoqa.pages.components.FileUploadComponent;
 import com.demoqa.pages.components.ResultsModalComponent;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -17,7 +18,13 @@ public class TestBase {
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
 
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
